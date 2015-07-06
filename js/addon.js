bee2f57eafc6408a5e95d20e570c95501c57b911
Eitan Isaacson <eitan@monotonous.org>
2015-07-06 12:52:21 -0700

    Make triple clicking more robust, and make addon disable/enable safe.

--- js/addon.js
+++ js/addon.js
@@ -1,20 +1,29 @@
 (function() {
 var clickCounter = 0;
 
-addEventListener("keypress", (e) => {
-  if (e.key === "MozCameraFocusAdjust") {
-    console.log('click', clickCounter);
+var quickFilterListener = (e) => {
+  if (e.key === "MozCameraFocusAdjust" || e.key === "Camera") {
     if (++clickCounter === 3) {
-      console.log('triple-click');
+      console.log('Toggle color filter');
       var lock = navigator.mozSettings.createLock();
       var req  = lock.get("accessibility.colors.enable");
       req.onsuccess = () => {
         lock.set({"accessibility.colors.enable": !req.result["accessibility.colors.enable"]});
-        document.body.hidden = true;
-        window.setTimeout(() => { document.body.hidden = false; }, 20);
+        document.body.style.transform = 'rotateZ(0deg)';
+        window.setTimeout(() => { document.body.style.transform = 'none'; }, 20);
       };
     }
-    setTimeout(() => { clickCounter-- }, 750);
+    setTimeout(() => { clickCounter-- }, 1000);
+  }
+};
+
+addEventListener("keypress", quickFilterListener);
+
+navigator.mozApps.mgmt.addEventListener("enabledstatechange", (e) => {
+  if (e.application.manifest.name === "Quick Filter" && !e.application.enabled) {
+    removeEventListener("keypress", quickFilterListener);
   }
 });
+
+
 })();
